71fa209012a2e9721bc7ba44691d8ce78fc6d378
Martxel <56430705+MartxelAran@users.noreply.github.com>
2024-09-26 18:25:30 +0200

    enhanced FractionTest #7

--- src/test/java/es/upm/miw/iwvg_devops/code/FractionTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/code/FractionTest.java
@@ -68,5 +68,54 @@ class FractionTest {
         });
         assertTrue(exception.getMessage().contains("/ by zero"));
     }
+
+    @Test
+    void isProperTest() {
+        Fraction fraction1 = new Fraction(2, 5);
+        assertTrue(fraction1.isProper());
+    }
+    @Test
+    void isImproperTest() {
+        Fraction fraction1 = new Fraction(12, 5);
+        assertTrue(fraction1.isImproper());
+    }
+    @Test
+    void isEquivalentTest() {
+        Fraction fraction1 = new Fraction(6, 5);
+        Fraction fraction2 = new Fraction(12, 10);
+        assertTrue(fraction1.isEquivalent(fraction2));
+        assertTrue(fraction2.isEquivalent(fraction1));
+    }
+    @Test
+    void addTest() {
+        Fraction fraction1 = new Fraction(1, 2);
+        Fraction fraction2 = new Fraction(3, 4);
+        Fraction result = fraction1.add(fraction2);
+        Fraction fraction = new Fraction(5, 4);
+        assertTrue(fraction.isEquivalent(result));
+    }
+    @Test
+    void multiplyTest() {
+        Fraction fraction1 = new Fraction(1, 2);
+        Fraction fraction2 = new Fraction(3, 4);
+        Fraction result = fraction1.multiply(fraction2);
+        Fraction fraction = new Fraction(3, 8);
+        assertTrue(fraction.isEquivalent(result));
+    }
+    @Test
+    void divideTest() {
+        Fraction fraction1 = new Fraction(1, 2);
+        Fraction fraction2 = new Fraction(3, 4);
+        Fraction result = fraction2.divide(fraction1);
+        assertEquals(new Fraction(6, 4).toString(), result.toString());
+    }
+    @Test
+    void divideByZeroTest() {
+        Fraction fraction1 = new Fraction(1, 2);
+        Fraction zeroFraction = new Fraction(0, 1);
+        assertThrows(IllegalArgumentException.class, () -> {
+            fraction1.divide(zeroFraction);
+        });
+    }
 }
 
